b2a22ae49a42d68e9034dea55e5eac20a2a53b52
dimitrispax <dimpax04@gmail.com>
2024-02-18 01:50:37 +0200

    Modified vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://dimitrispax.github.io/echoes-of-greece/',
   server: {
     proxy: {
       '/api': {
