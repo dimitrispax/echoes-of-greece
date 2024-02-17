a65af6542a25970ed37e546d381c9d8bb6c60097
dimitrispax <dimpax04@gmail.com>
2024-02-18 01:57:58 +0200

    Modified server URL.

--- src/services/quote.tsx
+++ src/services/quote.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = '/api'
+const baseUrl = 'https://greek-writers-quotes-api.onrender.com/api'
 
 /* Function that gets a Quote from the server */
 const getQuote = async () => {
